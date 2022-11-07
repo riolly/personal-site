75f27af0184dcf22365b881dc2d2a8c6f3ab09c3
riolly <alberto.riolly@gmail.com>
2022-11-07 09:31:28 +0700

    :arrow_up: chore: update to latest template v6.8

--- src/server/trpc/router/example.ts
+++ src/server/trpc/router/example.ts
@@ -1,12 +1,13 @@
-import {router, publicProcedure} from '../trpc'
 import {z} from 'zod'
+
+import {router, publicProcedure} from '../trpc'
 
 export const exampleRouter = router({
 	hello: publicProcedure
 		.input(z.object({text: z.string().nullish()}).nullish())
 		.query(({input}) => {
 			return {
-				greeting: `Hello ${input?.text ?? 'world'}`,
+				greeting: `Are you ready ${input?.text ?? 'challenger'}?`,
 			}
 		}),
 	getAll: publicProcedure.query(({ctx}) => {
